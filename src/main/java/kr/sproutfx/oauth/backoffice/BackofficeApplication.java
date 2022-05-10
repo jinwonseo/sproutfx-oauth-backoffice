b972404bcff86dc3d5a5201588195ad8c76590cb
Jinwon Seo <jwseo@live.co.kr>
2022-05-10 10:47:24 +0900

    Init empty spring boot project

---
+++ src/main/java/kr/sproutfx/oauth/backoffice/BackofficeApplication.java
@@ -0,0 +1,13 @@
+package kr.sproutfx.oauth.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackofficeApplication.class, args);
+	}
+
+}
