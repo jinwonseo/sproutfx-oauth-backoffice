590d8e2248528c962e6fdadbbfaeec167cc89213
Jinwon Seo <jwseo@live.co.kr>
2022-05-23 17:43:22 +0900

    Import 수정

--- src/main/java/kr/sproutfx/oauth/backoffice/api/project/controller/ProjectAuthorizeController.java
+++ src/main/java/kr/sproutfx/oauth/backoffice/api/project/controller/ProjectAuthorizeController.java
@@ -3,7 +3,10 @@ package kr.sproutfx.oauth.backoffice.api.project.controller;
 import kr.sproutfx.oauth.backoffice.api.project.entity.Project;
 import kr.sproutfx.oauth.backoffice.api.project.service.ProjectQueryService;
 import lombok.Data;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
