6eed8dcd77478ac1d39add99e78945f2f83742f5
Jinwon Seo <jwseo@live.co.kr>
2022-10-14 09:31:52 +0900

    반환유형 StructuredResponseEntity로 변경

--- src/main/java/kr/sproutfx/oauth/backoffice/common/advisor/RestControllerAdvisor.java
+++ src/main/java/kr/sproutfx/oauth/backoffice/common/advisor/RestControllerAdvisor.java
@@ -1,20 +1,21 @@
 package kr.sproutfx.oauth.backoffice.common.advisor;
 
-import kr.sproutfx.oauth.backoffice.common.base.BaseException;
-import kr.sproutfx.oauth.backoffice.common.dto.StructuredBody;
+import kr.sproutfx.oauth.backoffice.common.dto.response.StructuredResponseEntity;
+import kr.sproutfx.oauth.backoffice.common.exception.BaseException;
 import kr.sproutfx.oauth.backoffice.common.exception.UnhandledException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class RestControllerAdvisor {
     @ExceptionHandler(Throwable.class)
-    public ResponseEntity<StructuredBody> exception(final Throwable t) {
-        if (t instanceof RuntimeException && t.getCause() != null && t.getCause() instanceof BaseException) {
-            return ResponseEntity.status(((BaseException) t.getCause()).getHttpStatus()).body(StructuredBody.error(t.getCause()));
+    public StructuredResponseEntity exception(final Throwable t) {
+        if (t instanceof BaseException) {
+            return StructuredResponseEntity.error((BaseException) t);
+        } else if (t.getCause() != null && t.getCause() instanceof BaseException) {
+            return StructuredResponseEntity.error((BaseException) t.getCause());
         } else {
-            return ResponseEntity.internalServerError().body(StructuredBody.error(new UnhandledException(t.getLocalizedMessage())));
+            return StructuredResponseEntity.error(new UnhandledException(String.format("%s: %s", t.getClass().toString(), t.getLocalizedMessage())));
         }
     }
 }
